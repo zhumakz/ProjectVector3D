1ae925205bc63b901ee1797ef2cccf53ea68b92d
zhumakz <zhas12345@gmail.com>
2014-11-27 10:09:39 +0100

    Update TestVector3D.java

--- src/testing/TestVector3D.java
+++ src/testing/TestVector3D.java
@@ -1,35 +1,143 @@
-package testing;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+package vectors;
 
-import vectors.Vector3D;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-
-public class TestVector3D {
+/**
+ *
+ * @author student
+ */
+public class Vector3DTest {
     
-    public static void main(String[] args) {
-        Vector3D vectorU=new Vector3D();
-        Vector3D vectorV=new Vector3D(3, 5, 8);
-        Vector3D vectorW=new Vector3D(vectorV);
-        
-        System.out.println("vectorU="+vectorU);
-        System.out.println("vectorV="+vectorV);
-        System.out.println("vectorW="+vectorW);
-        
-        System.out.println("Długość vectorU="+vectorU.length());
-        System.out.println("Długość vectorV="+vectorV.length());
-        System.out.println("Długość vectorW="+vectorW.length());
-        
-        Vector3D vectorSum=vectorV.add(vectorW);
-        System.out.println("vectorSum="+vectorSum);
-        
-        Vector3D vectorMultiplied=vectorV.multiplyByScalar(2.5);
-        System.out.println("vectorMultiplied="+vectorMultiplied);
-        
-        double prod=vectorV.dotProduct(vectorW);
-        System.out.println("dotProduct="+prod);
-        
-        Vector3D vectorCrossProd=vectorV.crossProduct(vectorW);
-        System.out.println("vectorCrossProd="+vectorCrossProd);
-    }//public static void main
+    public Vector3DTest() {
+    }
     
-}//public class TestVector3D
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of length method, of class Vector3D.
+     */
+    @Test
+    public void testLength() {
+        System.out.println("length");
+        Vector3D instance = new Vector3D(2,3,4);
+        double expResult = Math.sqrt(2*2+3*3+4*4);
+        double result = instance.length();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+    }
+
+    /**
+     * Test of add method, of class Vector3D.
+     */
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        Vector3D anotherVector = new Vector3D(2,3,4);
+        Vector3D instance = new Vector3D(2,3,4);
+        Vector3D expResult = new Vector3D(2+2,3+3,4+4);
+        Vector3D result = instance.add(anotherVector);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+ 
+    }
+
+    /**
+     * Test of multiplyByScalar method, of class Vector3D.
+     */
+    @Test
+    public void testMultiplyByScalar() {
+        System.out.println("multiplyByScalar");
+        double alpha = 1.0;
+        Vector3D instance = new Vector3D(2,3,4);
+        Vector3D expResult = new Vector3D(2*1.0, 3*1.0, 4*1.0);
+        Vector3D result = instance.multiplyByScalar(alpha);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+
+    }
+
+    /**
+     * Test of dotProduct method, of class Vector3D.
+     */
+    @Test
+    public void testDotProduct() {
+        System.out.println("dotProduct");
+        Vector3D anotherVector = new Vector3D(2,3,4);
+        Vector3D instance = new Vector3D(2,3,4);
+        double expResult = (29);
+        double result = instance.dotProduct(anotherVector);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+      
+    }
+
+    /**
+     * Test of crossProduct method, of class Vector3D.
+     */
+    @Test
+    public void testCrossProduct() {
+        System.out.println("crossProduct");
+        Vector3D anotherVector = new Vector3D(1,2,3);
+        Vector3D instance = new Vector3D(5,6,7);
+        Vector3D expResult = new Vector3D (-1*(2*7-3*6),-1*(3*5-1*7),-1*(1*6-2*5));
+        Vector3D result = instance.crossProduct(anotherVector);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+       
+    }
+
+    /**
+     * Test of toString method, of class Vector3D.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        Vector3D instance = new Vector3D(2,3,4);
+        String expResult = "[2.0, 3.0, 4.0]";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+
+    }
+
+    /**
+     * Test of equals method, of class Vector3D.
+     */
+    @Test
+    public void testEquals() {
+        System.out.println("equals");
+        Object anotherObject = null;
+        Vector3D instance = new Vector3D();
+        boolean expResult = false;
+        boolean result = instance.equals(anotherObject);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+
+    }
+    
+}
